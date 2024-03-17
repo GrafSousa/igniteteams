1e59a69578d260d6f412674b05ba4fff762ffb70
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-17 20:45:32 -0300

    feat: add BackButton on the Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,18 @@ import * as S from './styles';
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+interface Props {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <S.Container>
+      {showBackButton && (
+        <S.BackButton>
+          <S.BackIcon />
+        </S.BackButton>
+      )}
       <S.Logo source={logoImg} />
     </S.Container>
   );
