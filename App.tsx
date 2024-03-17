1a3d4d27ffbccffdbfd6d2cd74c86c5b0fa7c862
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-17 18:34:44 -0300

    chore: add font Roboto

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { defaultTheme } from 'src/styles/themes/default';
 
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <SafeAreaView style={{ flex: 1 }}>
-        <Groups />
+        {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+
         <StatusBar style='dark' />
       </SafeAreaView>
     </ThemeProvider>
