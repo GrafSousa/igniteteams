008877d986dff58fe27e9bae6201d47829e2dd04
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-17 18:10:01 -0300

    feat: set the default theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import { defaultTheme } from 'src/styles/themes/default';
 
 import { Groups } from '@screens/Groups';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Groups />
-      <StatusBar style='dark' />
-    </SafeAreaView>
+    <ThemeProvider theme={defaultTheme}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Groups />
+        <StatusBar style='dark' />
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,29 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFF',
+
+    'green-500': '#00B37E',
+    'green-700': '#00875F',
+
+    red: '#F75A68',
+    'red-dark': '#AA2834',
+
+    'gray-100': '#E1E1E6',
+    'gray-200': '#C4C4CC',
+    'gray-300': '#7C7C8A',
+    'gray-400': '#323238',
+    'gray-500': '#29292E',
+    'gray-600': '#202024',
+    'gray-700': '#121214',
+  },
+  'font-family': {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold',
+  },
+  'font-size': {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+  },
+};
