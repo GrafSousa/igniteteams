4baa1133b8ee5289e9ce2883e9c49bdd8fe022d5
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-19 13:41:24 -0300

    feat: create list of GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,27 @@
-import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import * as S from './styles';
 
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <S.Container>
       <Header />
 
       <Highlight title='Turmas' subtitle='jogue com a sua turma' />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+
     </S.Container>
   );
 }
