fa9e4986d12ff7a2e0e0634b01cc6861e358b7fb
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-20 23:03:11 -0300

    feat: get all groups from AsyncStorage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 import * as S from './styles';
 
@@ -10,6 +10,8 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 
+import { groupsGetAll } from '@storage/group/groupsGetAll';
+
 export function Groups() {
   const navigation = useNavigation();
   const [groups, setGroups] = useState<string[]>([]);
@@ -17,6 +19,22 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <S.Container>
