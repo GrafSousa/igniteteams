b4f6d781ac3feb0ee7fc808a478ca4cc3074c5c1
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2024-03-20 23:15:25 -0300

    feat: handle exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
 import * as S from './styles';
 
+import { AppError } from '@utils/AppError';
+
+import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { Input } from '@components/Input';
 import { groupCreate } from '@storage/group/groupCreate';
+import { Alert } from 'react-native';
 
 export function NewGroup() {
   const navigation = useNavigation();
@@ -14,10 +18,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma');
+      }
       await groupCreate(group);
+
       navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo');
+        console.log(error);
+      }
     }
   }
 
